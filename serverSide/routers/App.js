4ebf3c2733952e6a601b292595c253e3230bbae6
Shalev Abadi <shalev787@gmail.com>
2018-08-19 17:47:56 +0300

    added error hadler

--- serverSide/routers/App.js
+++ serverSide/routers/App.js
@@ -5,6 +5,23 @@ const morgan = require('morgan');
 const drugRouter = require('../api/routers/drugs');
 
 app.use(morgan('dev'));
+
 app.use('/drugs', drugRouter);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status= 404;
+    next(error);
+});
+app.use('/drugs', drugRouter);
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
 
 module.exports = app;
