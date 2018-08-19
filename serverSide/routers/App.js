fbdc478d9723798e1803bf7672e2f51aac63ada8
Shalev Abadi <shalev787@gmail.com>
2018-08-19 17:13:14 +0300

    installed and added morgan in order to log

--- serverSide/routers/App.js
+++ serverSide/routers/App.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
 const drugRouter = require('../api/routers/drugs');
 
+app.use(morgan('dev'));
 app.use('/drugs', drugRouter);
 
 module.exports = app;
